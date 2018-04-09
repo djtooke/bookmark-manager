0e914e7e026c4570a17e7be1162eae77b7ead588
Daniel Tooke <daniel.tooke@gmail.com>
2018-04-09 14:43:02 +0100

    Added bookmark all method

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,5 +1,14 @@
 class Bookmark
 
-  
+    @@list = []
+
+  def self.all
+    @@list
+  end
+
+  def add
+    @@list << self
+  end
+
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,1 +1,13 @@
 require 'bookmark'
+
+describe Bookmark do
+  it 'should have a class method to retreive all bookmarks' do
+    bookmark1 = Bookmark.new
+    bookmark2 = Bookmark.new
+    bookmark1.add
+    bookmark2.add
+    expect(Bookmark.all).to eq([bookmark1, bookmark2])
+  end
+
+
+end
